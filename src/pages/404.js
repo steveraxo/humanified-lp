b0c0ff3b90ef0effa2e44f69ab7e81a661a23c8b
steveraxo <steve@raxo.tv>
2020-09-01 16:57:02 -0400

    fixed 4040 page message

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,9 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="Humanified | 404 Not found" />
-    <div className="not__found__page">
-      <h1>PAGE NOT FOUND</h1>
-      <p><a href="/">Go Back Home</a></p>
+    <div className="not__found__page text-center">
+      <h1 className="text-center">Oops! We want to make change happen, <br/> but it isn’t going to happen here.</h1>
+      <p><a href="/">Back to site</a></p>
     </div>
   </Layout>
 )
