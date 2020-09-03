6312643aec11a71ec8505975a6146221af790993
steveraxo <steve@raxo.tv>
2020-09-03 12:40:19 -0400

    fx

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,44 +33,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <script>
-        {`
-          var _ss = _ss || [];
-          _ss.push(['_setDomain', 'https://koi-3QNMLPDA8K.marketingautomation.services/net']);
-          _ss.push(['_setAccount', 'KOI-4AUKC53J2G']);
-          _ss.push(['_trackPageView']);
-          (function() {
-              var ss = document.createElement('script');
-              ss.type = 'text/javascript'; ss.async = true;
-              ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-3QNMLPDA8K.marketingautomation.services/client/ss.js?ver=2.4.0';
-              var scr = document.getElementsByTagName('script')[0];
-              scr.parentNode.insertBefore(ss, scr);
-          })();
 
-
-          var callThisOnReturn = function(resp) {
-            if (resp) {
-                var SharpSpringTracking = resp.trackingID;
-            }
-          };
-          _ss.push(['_setResponseCallback', callThisOnReturn]); 
-        `}
-      </script>
-
-      <script>
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window,document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '624158401859448');
-          fbq('track', 'PageView');
-          `}
-      </script>
       <Header siteTitle={data.site.siteMetadata.title} />
       <noscript>
         <img
