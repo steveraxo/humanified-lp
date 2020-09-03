15f7181004c202dbd5b598dfb87f3e559aff5c4a
steveraxo <steve@raxo.tv>
2020-09-03 12:37:10 -0400

    Added redirections

--- src/components/layout.js
+++ src/components/layout.js
@@ -40,11 +40,11 @@ const Layout = ({ children }) => {
           _ss.push(['_setAccount', 'KOI-4AUKC53J2G']);
           _ss.push(['_trackPageView']);
           (function() {
-          var ss = document.createElement('script');
-          ss.type = 'text/javascript'; ss.async = true;
-          ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-3QNMLPDA8K.marketingautomation.services/client/ss.js?ver=2.4.0';
-          var scr = document.getElementsByTagName('script')[0];
-          scr.parentNode.insertBefore(ss, scr);
+              var ss = document.createElement('script');
+              ss.type = 'text/javascript'; ss.async = true;
+              ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'koi-3QNMLPDA8K.marketingautomation.services/client/ss.js?ver=2.4.0';
+              var scr = document.getElementsByTagName('script')[0];
+              scr.parentNode.insertBefore(ss, scr);
           })();
 
 
